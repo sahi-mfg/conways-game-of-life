074e4607b809d62d4f578df763171f8a77d783bb
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-15 21:25:11 +0100

    feat: Add streamlit interface for Conway's Game of Life

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from src.grid import Grid
 import time
+import streamlit as st
 
 
-def delete_screen():
-    print("\033[H\033[J", end="")
+def format_grid(grid):
+    return "\n".join(" ".join(str(cell) for cell in row) for row in grid.grid)
 
 
 def main():
-    grid = Grid((20, 30))
-    grid.random_fill(50)
+    grid_size = st.slider("Grid size", min_value=10, max_value=100, value=20)
+    initial_live_cells = st.slider("Initial live cells", min_value=0, max_value=100, value=50)
+    grid = Grid((grid_size, grid_size))
+    grid.random_fill(initial_live_cells)
     grid.set_neighbors()
-    while True:
-        delete_screen()
-        print(grid)
-        print("\n")
-        time.sleep(0.5)
-        grid.game()
-        grid.actualize_grid()
+
+    iterations = st.sidebar.slider("Iterations", min_value=1, max_value=100, value=10)
+    pause = st.sidebar.checkbox("Pause")
+    speed = st.sidebar.slider("Speed", min_value=0.1, max_value=2.0, value=0.5, step=0.1)
+    if st.sidebar.button("Clear"):
+        grid = Grid((grid_size, grid_size))
+
+    for _ in range(iterations):
+        if not pause:
+            st.text(format_grid(grid))
+            time.sleep(speed)
+            grid.game()
+            grid.actualize_grid()
 
 
 if __name__ == "__main__":
+    st.title("Conway's Game of Life")
     main()
