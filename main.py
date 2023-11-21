a6b545e9746f23a99164a7793eb528fc4c69259a
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-21 05:37:32 +0100

    refactor: Remove Streamlit interface, revert to command-line representation of the game

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 from src.grid import Grid
 import time
-import streamlit as st
 
 
-def format_grid(grid):
-    return "\n".join(" ".join(str(cell) for cell in row) for row in grid.grid)
+def delete_screen():
+    print("\u001B[H\u001B[J")
 
 
 def main():
-    grid_size = st.slider("Grid size", min_value=10, max_value=100, value=20)
-    initial_live_cells = st.slider("Initial live cells", min_value=0, max_value=100, value=50)
-    grid = Grid((grid_size, grid_size))
-    grid.random_fill(initial_live_cells)
+    grid = Grid((50, 50))
+    grid.random_fill(50)
     grid.set_neighbors()
 
-    speed = st.sidebar.slider("Speed", min_value=0.1, max_value=2.0, value=0.5, step=0.1)
-    if st.sidebar.button("Clear"):
-        grid = Grid((grid_size, grid_size))
-
-    if st.sidebar.button("Random"):
-        grid.random_fill(initial_live_cells)
-        grid.set_neighbors()
-
-    if st.sidebar.button("Step"):
-        st.text(format_grid(grid))
-        time.sleep(speed)
+    print("Conway's Game of Life")
+    while True:
+        delete_screen()
+        print(grid)
+        print("\n")
+        time.sleep(0.8)
         grid.game()
         grid.actualize_grid()
 
 
 if __name__ == "__main__":
-    st.title("Conway's Game of Life")
     main()
--- src/cell.py
+++ src/cell.py
@@ -58,7 +58,7 @@ class Cell:
 
     def __str__(self):
         """the string representation of the cell."""
-        return "🧛🏽‍♀️" if self.current_state else "☠️"
+        return "X" if self.current_state else "-"
 
     def compute_future_state(self) -> None:
         """compute the future state of the cell."""
