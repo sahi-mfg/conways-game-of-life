d1ba59b15ebf1260cf5c14ca3c171ea31d452593
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-21 06:03:22 +0100

    feat: Allow the user the choose the number of initial cells alive

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def delete_screen():
 
 
 def main():
-    grid = Grid((30, 30))
-    grid.random_fill(50)
-    grid.set_neighbors()
-
     print("Conway's Game of Life")
-    num_iterations = int(input("After how many iteration should the game stop ?: "))
-    print("\n")
+    intial_cells_alive = int(input("Choose the number of intial cells alive: "))
+    num_iterations = int(input("After how many generations should the game stop ?: "))
     speed = float(input("Ajust the speed of the game. The number should be between 0.1 (faster) and 1 (slower): "))
+
+    grid = Grid((30, 30))
+    grid.random_fill(intial_cells_alive)
+    grid.set_neighbors()
 
     for _ in range(num_iterations):
         delete_screen()
