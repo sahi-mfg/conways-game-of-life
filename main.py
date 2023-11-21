a7b0436bf3c89237accbd0209a86148029f6a6f8
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-21 05:57:10 +0100

    feat: Added iteration and speed control to the game

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ def delete_screen():
 
 
 def main():
-    grid = Grid((50, 50))
+    grid = Grid((30, 30))
     grid.random_fill(50)
     grid.set_neighbors()
 
     print("Conway's Game of Life")
-    while True:
+    num_iterations = int(input("After how many iteration should the game stop ?: "))
+    print("\n")
+    speed = float(input("Ajust the speed of the game. The number should be between 0.1 (faster) and 1 (slower): "))
+
+    for _ in range(num_iterations):
         delete_screen()
         print(grid)
         print("\n")
-        time.sleep(0.8)
+        time.sleep(speed)
         grid.game()
         grid.actualize_grid()
+
+    print("Game stopped.")
 
 
 if __name__ == "__main__":
