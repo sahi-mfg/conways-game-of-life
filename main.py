48af76f4661f2f24276c454fc4af0d4c530cb7b6
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-12-03 03:41:04 +0100

    fixed typo

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def delete_screen():
 
 def main():
     print("Conway's Game of Life")
-    intial_cells_alive = int(input("Choose the number of intial cells alive: "))
+    initial_cells_alive = int(input("Choose the number of initial cells alive: "))
     num_iterations = int(input("After how many generations should the game stop ?: "))
-    speed = float(input("Ajust the speed of the game. The number should be between 0.1 (faster) and 1 (slower): "))
+    speed = float(input("Adjust the speed of the game. The number should be between 0.1 (faster) and 1 (slower): "))
 
     grid = Grid((30, 30))
-    grid.random_fill(intial_cells_alive)
+    grid.random_fill(initial_cells_alive)
     grid.set_neighbors()
 
     for _ in range(num_iterations):
